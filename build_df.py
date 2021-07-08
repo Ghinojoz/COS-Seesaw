5f12d6988b9e7493f1dd852001bc944f1f44e8b1
Ghinojoz <ghinojoz@ucsc.edu>
2021-07-07 20:07:29 -0700

    increase generalization
    Added generalization variables to be reused for dates, cos_target, timelag deltas. Changing these variables should change all functionality for the script. Time lags can be added arbitrarily using the relativedelta class from dateutil module

--- build_df.py
+++ build_df.py
@@ -21,17 +21,24 @@ import gc
 
 # ----------------------------------------------------------------------------------------------------------------------------------------
 # Generalization variables, modify the variables here to change targets, interpolation timelags, etc.
+# text divider for console output
+# ----------------------------------------------------------------------------------------------------------------------------------------
+divider = '--------------------------------------------------------------------------------------------------------'
 
 # COS target
 # Assumes that this target can be found in the OCS__GCMS_flask.txt file
-cos_target = 'CGO'
+cos_target = 'cgo'
 
-# SST timelags
-stt_delta_time = [relativedelta(months=-2), relativedelta(months=-1)]
+# target dates (inclusive)
+year_start = 2000
+year_end = 2018
 
+# generalized timelags, or SST timelags if other variables use different timelag periods 
+# store these as tuples , with the first element being the suffix to appear at then end of lagged data
+# and the second element representing the corresponding relativedelta from the dateutil module
+time_delta = [("-3m", relativedelta(months=-3)), ("-2m", relativedelta(months=-2)), ("-1m", relativedelta(months=-1))]
 
-
-
+# ----------------------------------------------------------------------------------------------------------------------------------------
 
 # can be applied to columns of dataframes, e.g.
 # decimal_years = my_data_frame['dd-mm-yyyy'].apply(toYearDecimal)
@@ -50,13 +57,18 @@ def constrainDataFrameByYears(df, dateColumn, startyear, endyear):
     return df[(df[dateColumn] >= dt(year=startyear, month=1, day=1)) & (df[dateColumn] < dt(year=endyear+1, month=1, day=1))]
 
 # regions must be a regions object which defines the desired regions
-def addSSTData(data_frame, regions, year_start, years):
+def addSSTData(data_frame, regions, start, end):
     # my_data_frame.reset_index(inplace=True, drop=True)
     data_frame.reset_index(inplace=True, drop=True)
-    # generate offset date array for interpolation
-    funct = lambda x: x + relativedelta(months=-2)
-    offset_date = my_data_frame['time'].apply(funct)
-    print(offset_date)
+
+    # ------------------------------------------------------------------------------------------------------------------------------------
+    # generate offset date arrays for interpolation
+    offset_dates = []
+    for delta in time_delta:
+        funct = lambda x: x + delta[1]
+        current_offset = my_data_frame['time'].apply(funct)
+        offset_dates.append((delta[0], current_offset))
+    # ------------------------------------------------------------------------------------------------------------------------------------
 
     # sst_dict is used to store sst_mean entries for each region. Regions are the key for the dictionary
     # each entry in the lists associated with a region key represents the sst_mean dataset for 1 year
@@ -66,8 +78,9 @@ def addSSTData(data_frame, regions, year_start, years):
         # add a column to our data frame for each region
         column_name = region.abbrev + '_sst'
         data_frame[column_name] = np.nan
-    for i in range(years):
-        f_name = './SourceData/sst/sst.day.mean.' + str(year_start + i) + '.nc'
+    # +1 to account for 0 starting index, otherwise ends at end - 1
+    for year in range(start, end + 1):
+        f_name = './SourceData/sst/sst.day.mean.' + str(year) + '.nc'
         print("Now processing " + f_name)
         noaa_data = xr.open_dataset(f_name).load()
         print('Done loading data')
@@ -84,41 +97,24 @@ def addSSTData(data_frame, regions, year_start, years):
         noaa_data.close()
         gc.collect()
         print('Done cleaning up after ' + f_name)
-        print('Year '+ str(2000 + i) + ' added successfully' )
+        print('Year '+ str(year) + ' added successfully' )
         print('-------------------------------------------------------------------------------------')
     # now the list of each regions' yearly mean must be combined into a single dataset
     for region in sst_dict.keys():
         sst_dict[region] = xr.concat(sst_dict[region], dim='time')
-        sst_dict[region].sst.plot(label=region)
         interp = sst_dict[region].sst.interp(time=list(map(str, data_frame['time'])), method='cubic')
-        print(interp)
         interp.to_dataframe()
-        print(interp)
-        #interp.reset_index(inplace=True, drop=True)
-        #interp.reset_index(drop=True)
         column_name = region + '_sst'
-        label = region + '_interp'
-        #interp.rename({'sst': column_name})
-        #print(interp)
-        #interp.columns = [column_name]
-        #interp[column_name].plot.scatter(x='time', y=column_name, label=label)
-        interp.plot(label=label, marker = 'o')
         data_frame[column_name] = interp
 
-        # add the date offset interpolation for each region
-        column_name = column_name + '-2m'
-        interp = sst_dict[region].sst.interp(time=list(map(str, offset_date)), method='cubic')
-        print(interp)
-        #interp.to_dataframe()
-        label = label + '-2m'
-        interp.plot(label=label, marker = 's')
-        data_frame[column_name] = interp
-
-    plt.legend()
-    plt.show()
+        # add the date offset interpolation for each offset and each region
+        for delta in offset_dates:
+            column_name = region + '_sst' + delta[0]
+            interp = sst_dict[region].sst.interp(time=list(map(str, delta[1])), method='cubic')
+            data_frame[column_name] = interp
 
     return data_frame
-
+'''
 my_data_frame = pd.DataFrame()
 # Read jungfraujoch data and constrain to years between 2000-2018, convert dates to decimal time if necessary
 print("retrieving jfj_data, and constraining to 2000-2018")
@@ -128,19 +124,31 @@ print("success")
 
 # strip down to just cos, cos_stdev and time
 jfj_data = pd.DataFrame({'time': jfj_data['dd-mmm-yyyy'], 'COS_JFJ': jfj_data['ppt']})
+'''
 
-# Read manua load data from OCS_GCMS_flask.txt and constrain to years between 2000-2018, convert dates to decimal times
-print('load mlo data and constrain to 2000-2018')
-mlo_data = pd.read_csv('./SourceData/OCS__GCMS_flask.txt', delim_whitespace=True, header=1, parse_dates=['yyyymmdd'])
-mlo_data = mlo_data.loc[mlo_data['site'] == 'mlo']
-mlo_data = constrainDataFrameByYears(mlo_data, 'yyyymmdd', 2000, 2018)
-print('success')
+# to modify the COS target, simply change the cos_target variable at the top of the script
+my_data_frame = pd.DataFrame()
+print(divider)
+print('load COS data and constrain to desired years: ' + str(year_start) + '-' + str(year_end))
+cos_data = pd.read_csv('./SourceData/OCS__GCMS_flask.txt', delim_whitespace=True, header=1, parse_dates=['yyyymmdd'])
+cos_data = cos_data.loc[cos_data['site'] == cos_target]
+cos_data = constrainDataFrameByYears(cos_data, 'yyyymmdd', year_start, year_end)
 
-my_data_frame = jfj_data
-#delete COS sets
-del [[jfj_data, mlo_data]]
+# strip and add to dataframe
+cos_column_name = 'COS_' + cos_target
+cos_data = pd.DataFrame({'time': cos_data['yyyymmdd'], cos_column_name: cos_data['OCS_']})
+my_data_frame = cos_data
+print('Success!')
+print('Result:')
+print(my_data_frame)
+
+print(divider)
+# free up space and collect garbage
+print('Free cos_data space and collect garbage')
+del[[cos_data]]
 gc.collect()
-print('--------------------------------------------------------------------')
+
+print('Success!')
 
 # generate our ocean regions
 north_hemisphere = np.array([[-180, 0], [-180, 90], [180, 90], [180, 0]])
@@ -150,8 +158,16 @@ abbrevs = ['NH', 'SH']
 regions = regionmask.Regions([north_hemisphere, south_hemisphere], names=names, abbrevs=abbrevs, name='Hemispheres')
 
 #regions = regionmask.defined_regions.ar6.ocean
+print(divider)
+print('Begin adding sst data for year range: ' + str(year_start) + '-' + str(year_end))
+print(divider)
+print('With the following timelags:')
+for lag in time_delta:
+    print(lag)
+print(divider)
 
-my_data_frame = addSSTData(my_data_frame, regions, 2000, 19)
+
+my_data_frame = addSSTData(my_data_frame, regions, year_start, year_end)
 print(my_data_frame)
 
 if my_data_frame.isnull().values.any():
